385f7746641b5fa3cd4f42673a6734676763c413
PanAndrey <panadr09@gmail.com>
2021-11-06 21:51:56 +0400

    feat: add 04-copy-directory solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,7 @@
 const path = require("path");
 const fs = require("fs");
-const process = require("process");
 
 let dirPath = path.join(__dirname, "/secret-folder");
-
-// import { readdir } from "fs/promises";
-
-// try {
-//   const files = await readdir(dirPath);
-//   for (const file of files) console.log(file);
-// } catch (err) {
-
-//   console.error(err);
-// }
 
 function getFiles(dirPath) {
   fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require("fs");
+const path = require("path");
 
+const copyDir = path.join(__dirname, "/files-copy");
+const originalDir = path.join(__dirname, "/files");
+
+fs.mkdir(copyDir, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+fs.readdir(originalDir, (err, files) => {
+  files.forEach((file) => {
+    fs.copyFile(
+      path.join(originalDir, file),
+      path.join(copyDir, file),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  });
+});
